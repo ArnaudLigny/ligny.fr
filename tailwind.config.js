1b1f049657022df833cca50f0e5f60438a51c559
Arnaud Ligny <arnaud@ligny.org>
2024-02-10 00:47:55 +0100

    Update CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ module.exports = {
     extend: {
       colors: {
         // https://tailwindcss.com/docs/customizing-colors
-        primary: colors.sky,
+        //primary: colors.sky,
+        primary: {
+          50: "#EBF3FE",
+          100: "#D8E6FD",
+          200: "#B1CEFB",
+          300: "#8AB5FA",
+          400: "#639CF8",
+          500: "#3B82F6",
+          600: "#0B60EA",
+          700: "#0848B0",
+          800: "#053075",
+          900: "#03183B",
+          950: "#010C1D"
+        },
         neutral: colors.slate,
         social: {
           twitter: '#1da1f2',
